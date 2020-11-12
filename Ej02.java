301bad9996be4c0a937d121b8f9ccac1ac37d240
Lucia Muñoz <lucia.munoz86@educa.madrid.org>
2020-11-12 16:33:48 +0100

    Primera version

---
+++ Ej02.java
@@ -0,0 +1,19 @@
+package Sesion03;
+import java.util.Scanner;
+public class Ej02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el primer número");
+        int num1 = sc.nextInt();
+        boolean enRango = num1 < 10 && num1 > 0;
+        System.out.println(enRango);
+        System.out.println("Introduce el segundo número");
+        int num2 = sc.nextInt();
+        System.out.println("Introduce el tercer número");
+        int num3 = sc.nextInt();
+
+
+    }
+
+    }
